3f71847840968b7fe15e76788b0306cf2007797e
Ghost.Gold <Ghost.Gold@fb7abace-b6b7-eadf-bba5-f4d734e93118>
2011-02-02 19:03:52 +0000

    Remove All

--- trunk/Absyn/LetExp.java
+++
@@ -1,7 +0,0 @@
-package Absyn;
-import Symbol.Symbol;
-public class LetExp extends Exp {
-   public DecList decs;
-   public ExpList body;
-   public LetExp(int p, DecList d, ExpList b) {pos=p; decs=d; body=b;}
-}
