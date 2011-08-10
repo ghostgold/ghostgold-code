53b286249fbaa4c18d6c8e47e7fb8da69092d0ad
Ghost.Gold <Ghost.Gold@fb7abace-b6b7-eadf-bba5-f4d734e93118>
2011-08-10 01:58:27 +0000

    join and condition2

--- trunk/nachos/src/nachos/threads/Condition2.java
+++ trunk/nachos/src/nachos/threads/Condition2.java
@@ -1,5 +1,7 @@
 package nachos.threads;
 
+import java.util.LinkedList;
+
 import nachos.machine.*;
 
 /**
@@ -22,6 +24,7 @@ public class Condition2 {
 	 */
 	public Condition2(Lock conditionLock) {
 		this.conditionLock = conditionLock;
+		waitQueue = new LinkedList<KThread>();
 	}
 
 	/**
@@ -32,9 +35,15 @@ public class Condition2 {
 	 */
 	public void sleep() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-
+		
+		waitQueue.add(KThread.currentThread());
+		
 		conditionLock.release();
-
+		
+		boolean intStatus = Machine.interrupt().disable();
+		KThread.sleep();
+		Machine.interrupt().restore(intStatus);
+		
 		conditionLock.acquire();
 	}
 
@@ -44,6 +53,12 @@ public class Condition2 {
 	 */
 	public void wake() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		
+		if (!waitQueue.isEmpty()) {
+			boolean intStatus = Machine.interrupt().disable();
+			waitQueue.removeFirst().ready();
+			Machine.interrupt().restore(intStatus);
+		}
 	}
 
 	/**
@@ -52,7 +67,10 @@ public class Condition2 {
 	 */
 	public void wakeAll() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		while (!waitQueue.isEmpty())
+			wake();
 	}
 
 	private Lock conditionLock;
+	private LinkedList<KThread> waitQueue;
 }
