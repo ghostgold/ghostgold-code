39ea64cbbdfc4e28e057ad7b2dd46782a6c605a2
Ghost.Gold <Ghost.Gold@fb7abace-b6b7-eadf-bba5-f4d734e93118>
2011-11-23 14:35:03 +0000

    phase2-task4 lottery scheduler

--- trunk/nachos/src/nachos/threads/LotteryScheduler.java
+++ trunk/nachos/src/nachos/threads/LotteryScheduler.java
@@ -1,5 +1,7 @@
 package nachos.threads;
 
+import nachos.machine.Lib;
+
 /**
  * A scheduler that chooses threads using a lottery.
  * 
@@ -36,7 +38,112 @@ public class LotteryScheduler extends PriorityScheduler {
 	 * @return a new lottery thread queue.
 	 */
 	public ThreadQueue newThreadQueue(boolean transferPriority) {
-		// implement me
-		return null;
+		return new LotteryQueue(transferPriority); 
+
 	}
+	protected ThreadState getThreadState(KThread thread) {
+		if (thread.schedulingState == null)
+			thread.schedulingState = new LotteryState(thread);
+
+		return (LotteryState) thread.schedulingState;
+	}
+
+	@Override 
+	public int getPriorityMaximum() {
+		return priorityMaximum;
+	}
+	public int getPriorityMinimum() {
+		return priorityMinimum;
+	}
+	
+	protected class LotteryQueue extends PriorityQueue {
+		public LotteryQueue (boolean tranferPriority) {
+			super(tranferPriority);
+		}
+		
+		@Override
+		public KThread nextThread() {
+			if (observer != null) 
+				observer.unRegister(this);
+
+			if (waitQueue.isEmpty())
+				return null;
+			int lottery = Lib.random(cachedDonation);
+			ThreadState next = null;
+			
+
+			for (ThreadState t: waitQueue) {
+				if (lottery < t.getEffectivePriority()) {
+					next = t;
+					break;
+				}
+				lottery -= t.getEffectivePriority();
+			}
+			
+			if (next == null) 
+				return null;
+			waitQueue.remove(next);
+			next.setObserver(null);
+			this.cachedDonation = this.calcDonation();
+			next.acquire(this);
+			Lib.debug('p', next.thread + "leave queue " + id);
+			return next.thread;
+		}
+		
+		@Override
+		public int getDonation() {
+			if (transferPriority)
+				return cachedDonation;
+			else 
+				return 0;
+		}
+		
+		@Override 
+		public void update() {
+			cachedDonation = calcDonation();
+			if (transferPriority && observer != null) {
+				observer.update();
+			}
+		}
+		
+		int calcDonation() {
+			int temp = 0;
+			for(ThreadState t: waitQueue) {
+				temp += t.getEffectivePriority();
+			} 
+			return temp;
+		}	
+		
+		int cachedDonation = 0;
+	}
+	
+	protected class LotteryState extends ThreadState {
+		public LotteryState (KThread thread) {
+			super(thread);
+			this.donation = 0;
+		}
+		
+		@Override 
+		public void update() {
+			donation = 0;
+			for (PriorityQueue queue: servents) {
+				donation += queue.getDonation();
+			}
+			if (observer != null)
+				observer.update();
+		}
+		@Override 
+		public int getEffectivePriority() {
+			return priority + donation;
+		}
+	}
+	/**
+	 * The minimum priority that a thread can have. Do not change this value.
+	 */
+	public static final int priorityMinimum = 0;
+	/**
+	 * The maximum priority that a thread can have. Do not change this value.
+	 */
+	public static final int priorityMaximum = Integer.MAX_VALUE;
+
 }
